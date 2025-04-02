8c18b9b4417280ccf4b06f6c5b8905981b1498a0
minyeok <alsgurgud@naver.com>
2025-04-02 14:13:07 +0900

    js basic study

--- scripts/main.js
+++ scripts/main.js
@@ -164,3 +164,35 @@ myInput.addEventListener('keydown', function(e){
     // console.log(e)
     console.log(myInput.value)
 })
+
+// 비동기(callback 함수)
+console.log('hi')
+setTimeout(function(){console.log('late')},1000)
+console.log('bye')
+
+// request
+const URL = 'https://jsonplaceholder.typicode.com/todos/1'
+
+
+// 비동기 처리 방법 1 (primise)
+
+let response = fetch(URL)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+// 비동기 처리 방법 2 (async await)
+async function fetchTodo(url) {
+    let res = await fetch(url)
+    let result = await res.json()
+    console.log(result)
+}
+console.log(fetchTodo(URL))
+
+let liArray = document.querySelectorAll('li')
+// console.log(liArray)
+
+liArray.forEach(function(item){
+    item.addEventListener('click', function(e){
+        console.log(e.target)
+    })
+})
